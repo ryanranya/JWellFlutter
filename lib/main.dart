86bdb67e9cc0acf3009615c2048a5c21248cbee2
ryanranya <ryanranya@163.com>
2020-05-08 23:09:37 +0800

    加减，按钮状态

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-main() =>runApp(MyApp());
+main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -29,14 +29,42 @@ class RYHomePageCountent extends StatefulWidget {
 }
 
 class _RYHomePageCountentState extends State<RYHomePageCountent> {
+
+  int _counter = 0;
   @override
   Widget build(BuildContext context) {
     return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+         _getButtons(),
+          Text("当前计数:$_counter")
+        ],
+      ),
+    );
+  }
 
-    )
+  Widget _getButtons(){
+    return  Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        RaisedButton(
+            child: Icon(Icons.add),
+            color: Colors.orange,
+            onPressed: () {
+              setState(() {
+                _counter += 1;
+              });
+            }),
+        RaisedButton(
+            child: Icon(Icons.remove),
+            color: Colors.blue,
+            onPressed: () {
+              setState(() {
+                _counter -= 1;
+              });
+            }),
+      ],
+    );
   }
 }
-
-
-
-
