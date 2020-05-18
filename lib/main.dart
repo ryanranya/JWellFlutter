2b4825340ac5f7f2c0363730fc3679d5f182ad81
ryanranya <ryanranya@163.com>
2020-05-18 13:35:20 +0800

    生命周期

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,10 @@ class RYHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("JWell Flutter"),
-        ),
-        body: RYHomePageCountent(),
+      appBar: AppBar(
+        title: Text("JWell Flutter"),
+      ),
+      body: RYHomePageCountent(),
     );
   }
 }
@@ -41,25 +41,67 @@ class RYHomePage extends StatelessWidget {
 
 //StatefulWidget生命周期
 class RYHomePageCountent extends StatefulWidget {
+  RYHomePageCountent() {
+    print("1、调用RYHomePageCountent的Constructor");
+  }
+
   @override
-  _RYHomePageCountentState createState() => _RYHomePageCountentState();
+  _RYHomePageCountentState createState() {
+    print("2、调用RYHomePageCountent的CreateState的方法");
+    return _RYHomePageCountentState();
+  }
 }
 
 class _RYHomePageCountentState extends State<RYHomePageCountent> {
-  int _counter = 0;
+  var _counter = 0;
+
+  _RYHomePageCountentState() {
+    print("3、调用_RYHomePageCountentState的constructor方法");
+  }
+
+  @override
+//  这里必须掉super进行初始化
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("4、调用initState方法进行初始化操作");
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    print("调用了didChangeDependencies");
+  }
+
+  @override
+  void didUpdateWidget(RYHomePageCountent oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    print("调用了didUpdateWidget");
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print("6、销毁方法");
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "当前计数:$_counter",
-            style: TextStyle(fontSize: 30, color: Colors.orange),
-          ),
-        ],
-      ),
+    print("5、调用build方法");
+    return Column(
+      children: <Widget>[
+        RaisedButton(
+          onPressed: () {
+            setState(() {
+              _counter += 1;
+            });
+          },
+          child: Icon(Icons.add),
+        ),
+        Text("当前点击$_counter"),
+      ],
     );
   }
 }
