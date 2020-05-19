6efd09f314d1fea5d34e942ae038fac5b63ac01e
ryanranya <ryanranya@163.com>
2020-05-19 20:49:43 +0800

    imageView

---
+++ lib/03day_image.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: RYHomePage(),
+    );
+  }
+}
+
+class RYHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("JWell Flutter"),
+      ),
+      body: RYHomeContent(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("FloatingActionButton");
+        },
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class RYHomeContent extends StatefulWidget {
+  @override
+  _RYHomeContentState createState() => _RYHomeContentState();
+}
+
+class _RYHomeContentState extends State<RYHomeContent> {
+  final imageUrl =
+      "https://oimagea7.ydstatic.com/image?id=2223816700015246366&product=adpublish&w=520&h=347";
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Image(
+          image: NetworkImage(imageUrl),
+          fit: BoxFit.contain,
+          repeat: ImageRepeat.repeatY,
+          width: 200,
+          height: 200,
+//      alignment: Alignment.center,
+          alignment: Alignment(0, -1),
+        ),
+        Image(
+          image: AssetImage("assets/images/timg.jpeg"),
+        )
+      ],
+    );
+  }
+}
