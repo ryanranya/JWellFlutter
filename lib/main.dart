6dab319000b6eb06e812b180fa2a931a9350af0c
ryanranya <ryanranya@163.com>
2020-05-19 20:51:33 +0800

    imageView

--- lib/main.dart
+++ lib/main.dart
@@ -38,31 +38,6 @@ class RYHomeContent extends StatefulWidget {
 class _RYHomeContentState extends State<RYHomeContent> {
   @override
   Widget build(BuildContext context) {
-    return item();
-  }
-}
-
-class item extends StatefulWidget {
-  @override
-  _itemState createState() => _itemState();
-}
-
-class _itemState extends State<item> {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        Row(
-          children: <Widget>[
-            Text("码单"),
-            Text(
-              "攀枝花钢业集团寄单号阿斯利康黑龙江看哈VB自行车阿开发商的老客户放假卡萨丁",
-              textAlign: TextAlign.end,
-              maxLines: 5,
-            ),
-          ],
-        )
-      ],
-    );
+    return Container();
   }
 }
