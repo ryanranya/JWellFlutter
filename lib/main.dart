67e9b64de69b53fbe943f5132e3d3931ac5ae3c4
ryanranya <ryanranya@163.com>
2020-05-19 09:33:54 +0800

    Expanded包裹Row里面的Text才能换行

--- lib/main.dart
+++ lib/main.dart
@@ -38,23 +38,58 @@ class RYHomeContent extends StatefulWidget {
 class _RYHomeContentState extends State<RYHomeContent> {
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: <Widget>[
-        Text(
-          "123123",
-          style: TextStyle(fontSize: 30, color: Colors.orange),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(10),
-            child: Text(
-              "XH12233213123123123213123123213",
-              style: TextStyle(fontSize: 35, color: Colors.lightBlue),
-              maxLines: 3,
+        Row(
+          children: <Widget>[
+            Text(
+              "123123",
+              style: TextStyle(fontSize: 30, color: Colors.orange),
             ),
-          ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  "XH12233213123123123213123123213",
+                  style: TextStyle(fontSize: 35, color: Colors.lightBlue),
+                  maxLines: 3,
+                ),
+              ),
+            ),
+          ],
         ),
+        JWellButton(),
       ],
+    );
+  }
+}
+
+class JWellButton extends StatefulWidget {
+  @override
+  _JWellButtonState createState() => _JWellButtonState();
+}
+
+class _JWellButtonState extends State<JWellButton> {
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      color: Colors.orange,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      onPressed: () {
+        print("Click JWellButton");
+      },
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Icon(
+            Icons.add,
+            color: Colors.red,
+          ),
+          Text("JWellButton"),
+        ],
+      ),
     );
   }
 }
