ea497e15567e6a18f53cde9ef3752e64a86df217
ryanranya <ryanranya@163.com>
2020-05-21 08:20:24 +0800

    Flexible

--- lib/main.dart
+++ lib/main.dart
@@ -31,19 +31,134 @@ class RYHomePage extends StatelessWidget {
 }
 
 class RYHomeContent extends StatefulWidget {
-
   @override
   _RYHomeContentState createState() => _RYHomeContentState();
 }
 
 class _RYHomeContentState extends State<RYHomeContent> {
-  final userNameTextEditController = TextEditingController();
-  final passWordTextEditController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
-    return  Container(
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+//      mainAxisSize: MainAxisSize.min,
+      verticalDirection: VerticalDirection.down,
+      textBaseline: TextBaseline.ideographic,
+      children: <Widget>[
+        Flexible(
+          fit: FlexFit.tight,
+          flex: 1,
+          child: Container(
+            width: 80,
+            height: 60,
+            color: Colors.orange,
+            child: Text(
+              "Hello",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ),
+        Flexible(
+          fit: FlexFit.tight,
+          flex: 2,
+          child: Container(
+            width: 120,
+            height: 100,
+            color: Colors.red,
+            child: Text(
+              "word",
+              style: TextStyle(fontSize: 30),
+            ),
+          ),
+        ),
+        Container(
+          width: 90,
+          height: 80,
+          color: Colors.blue,
+          child: Text(
+            "ABC",
+            style: TextStyle(fontSize: 12),
+          ),
+        ),
+        Container(
+          width: 50,
+          height: 120,
+          color: Colors.pinkAccent,
+          child: Text(
+            "CBA",
+            style: TextStyle(fontSize: 40),
+          ),
+        ),
+      ],
+    );
+  }
+}
 
+class RowDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        textBaseline: TextBaseline.ideographic,
+        children: <Widget>[
+          Container(
+            width: 80,
+            height: 60,
+            color: Colors.orange,
+            child: Text(
+              "Hello",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          Container(
+            width: 120,
+            height: 100,
+            color: Colors.red,
+            child: Text(
+              "word",
+              style: TextStyle(fontSize: 30),
+            ),
+          ),
+          Container(
+            width: 90,
+            height: 80,
+            color: Colors.blue,
+            child: Text(
+              "ABC",
+              style: TextStyle(fontSize: 12),
+            ),
+          ),
+          Container(
+            width: 50,
+            height: 120,
+            color: Colors.pinkAccent,
+            child: Text(
+              "CBA",
+              style: TextStyle(fontSize: 40),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonRaised extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {},
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Icon(Icons.add),
+          Text("Bug报告"),
+        ],
+      ),
     );
   }
 }
