827496e8046f66d80ce19e39d54ceb3ac8e8a67f
ryanranya <ryanranya@163.com>
2020-05-27 08:18:04 +0800

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -38,17 +38,50 @@ class RYHomeContent extends StatefulWidget {
 class _RYHomeContentState extends State<RYHomeContent> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: List.generate(100, (index){
-        return ListTile(
-          leading: Icon(Icons.people),
-          trailing: Icon(Icons.delete),
-          title: Text("联系人${index + 1}"),
-          subtitle: Text("联系人电话号码：13452707225"),
+    return ListView.separated(
+
+        itemBuilder: (BuildContext ctx, int index) {
+          return Divider(
+            color: Colors.orange,
+            height: 3,
+            thickness: 5,
+          );
+        },
+        separatorBuilder: (BuildContext ctx, int index) {
+          return Text("Hello Jwell",style: TextStyle(fontSize: 20),);
+        },
+        itemCount: 100);
+  }
+}
+
+class ListView02 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 100,
+      itemExtent: 60,
+      itemBuilder: (BuildContext ctx, int index) {
+        return Text(
+          "Hello JWell",
+          style: TextStyle(fontSize: 20),
         );
-      })
+      },
     );
   }
 }
 
-
+class ListViewO1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+//      itemExtent: 100,
+        children: List.generate(100, (index) {
+      return ListTile(
+        leading: Icon(Icons.people),
+        trailing: Icon(Icons.delete),
+        title: Text("联系人${index + 1}"),
+        subtitle: Text("联系人电话号码：13452707225"),
+      );
+    }));
+  }
+}
